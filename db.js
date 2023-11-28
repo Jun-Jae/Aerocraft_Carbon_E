cfcc859ef1a1f998a279137c55e5715628cc5826
JunJae <wnswo7960@naver.com>
2023-11-29 02:07:08 +0900

    v5

--- db.js
+++ db.js
@@ -20,53 +20,22 @@ nunjucks.configure("views", {
 
 //mongodb connect
 mongoose
-  .connect("mongodb://127.0.0.1:27017/carbon_emissions")
-  .then(() => console.log("DB 연결 성공"))
-  .catch(e => console.log(err));
+  .connect("mongodb://localhost:27017/carbon_emissions")
+  .then(() => console.log("connected DB Successfully"))
+  .catch((err) => console.log(err));
 
-//mondodb set
+//mondodb setting
 const { Schema } = mongoose;
 
-const InsertingSchema = new Schema ({ // Insert 스키마 구조 
-  Model : String,
-  tonCO2eq :  {
-    type : Number,
-    default : 0,
-  }
-})
-
-// Inserting Model의 경우 기존의 emission_factorSchema에 병합
-
-// Insert를 위한 코드 작성
-app.get('/insert', (req, res) => {
-  res.render('insert');
-});
-app.post('/insert', async (req, res) => {
-  // request 안에 있는 내용을 처리
-  // request.body
-  const insertYear = req.body.insertYear;
-  const insertData = req.body.insertData;
-  
-  // mongodb에 저장
-  const inserting = emission_factor({
-    insertYear : insertYear,
-    insertData : insertData
-  })
-  const result = await inserting.save().then(() => { // 객체에 생성된 data 저장
-      console.log('Success')
-      //res.render('detail', {title: title, contents: contents });
-  }) .catch((err) => {
-    console.error(err)
-    res.render('insert')
-  })
-});
-
-const emission_factorSchema = new Schema({ // 스키마의 구조 선언
+const emission_factorSchema = new Schema({
   Model: String,
   tonCO2eq: Number,
 });
-
-const emission_factor = mongoose.model("emission_factor",emission_factorSchema,"emission_factor");
+const emission_factor = mongoose.model(
+  "emission_factor",
+  emission_factorSchema,
+  "emission_factor"
+);
 
 const Gas_emission_yearSchema = new Schema({
   year: Number,
@@ -78,6 +47,32 @@ const Gas_emission_year = mongoose.model(
   "Greenhouse_Gas_Emissions_by_Year"
 );
 
+app.post("/insert", async (req, res) => {
+  // request 안에 있는 내용을 처리
+  // request.body
+  const name = req.body.Name;
+  const co2 = req.body.co2;
+
+  console.log(name);
+  console.log(co2);
+  // mongodb에 저장
+  const inserting = emission_factor({
+    Model: name,
+    tonCO2eq: co2,
+  });
+  const result = await inserting
+    .save()
+    .then(() => {
+      // 객체에 생성된 data 저장
+      console.log("Success");
+      res.redirect("/");
+      //res.render('detail', {title: title, contents: contents });
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+});
+
 app.get("/", async (req, res) => {
   //print chart
   try {
@@ -87,9 +82,8 @@ app.get("/", async (req, res) => {
     let chartAvg = await emission_factor.aggregate([
       { $group: { _id: null, averageValue: { $avg: "$tonCO2eq" } } },
     ]);
-
-    console.log(chartValue); // 결과를 확인하기 위해 로그로 출력
-    console.log(chartAvg);
+    //console.log(chartValue); // 결과를 확인하기 위해 로그로 출력
+    //console.log(chartAvg);
 
     // JSON 데이터를 문자열로 변환하여 Nunjucks에 전달
     res.render("main", {
@@ -101,6 +95,75 @@ app.get("/", async (req, res) => {
     console.error(err);
     res.status(500).send("Internal Server Error");
   }
+});
+
+app.post("/", async (req, res) => {
+  try {
+    const model = req.body.model;
+
+    // emission_factor.find()는 비동기 함수이므로 await를 사용
+    const modelValue = await emission_factor.findOne({ Model: model });
+    // find가 성공하면 modelValue에 데이터가 들어있을 것이므로 그 값을 로그에 출력하고, detail 템플릿으로 전달
+    console.log(modelValue);
+    res.render("detail", { modelValue: modelValue });
+  } catch (error) {
+    // find에서 에러가 발생하면 catch 블록으로 이동하여 에러를 출력
+    console.error(error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+app.get("/detail/:model", async (req, res) => {
+  try {
+    const model = req.params.model;
+    const modelValue = await emission_factor.findOne({ Model: model });
+    console.log(modelValue, " Success");
+    let chartAvg = await emission_factor.aggregate([
+      { $group: { _id: null, averageValue: { $avg: "$tonCO2eq" } } },
+    ]);
+    console.log(chartAvg);
+
+    res.render("detail", { modelValue: modelValue });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+app.post("/delete/:id", async (req, res) => {
+  const id = req.params.id;
+  const deleteValue = emission_factor
+    .deleteOne({ _id: id })
+    .then(() => {
+      console.log("delete Success : ", deleteValue);
+      res.redirect("/");
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+});
+
+app.post("/edit", async (req, res) => {
+  const updateId = req.body.updateId;
+  const updatemodelName = req.body.updateModelValue;
+  const updatetonCO2eq = req.body.updateCO2Value;
+
+  var updateValue = {
+    $set: {
+      Model: updatemodelName,
+      tonCO2eq: updatetonCO2eq,
+    },
+  };
+
+  let update = await emission_factor
+    .updateOne({ _id: updateId }, updateValue)
+    .then(() => {
+      console.log("success");
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+  res.redirect("/");
 });
 
 app.get("/year", async (req, res) => {
